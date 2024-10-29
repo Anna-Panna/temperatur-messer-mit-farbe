820ec7f70172c8eeb26a122b69c2d07360a3a255
Anna-Panna <anna.m.neumeier@icloud.com>
2024-10-29 10:35:21 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
-basic.forever(function () {
-	
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    basic.showString("TEMERATUR")
+    if (input.temperature() < 20) {
+        basic.setLedColors(0x0000ff, 0x0000ff, 0x0000ff)
+    } else if (input.temperature() > 20) {
+        basic.setLedColors(0xff0000, 0xff0000, 0xff0000)
+    }
 })
