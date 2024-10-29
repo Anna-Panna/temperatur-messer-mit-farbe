007c413086941b904b4a32f21dbe2d5a26d6fe41
Anna-Panna <anna.m.neumeier@icloud.com>
2024-10-29 10:48:39 +0100

    Musik ein gebaut

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    basic.showString("TEMERATUR")
+    basic.showString("" + (input.temperature()))
     if (input.temperature() < 20) {
         basic.setLedColors(0x0000ff, 0x0000ff, 0x0000ff)
+        music.play(music.stringPlayable("B A G A G F A C5 ", 120), music.PlaybackMode.UntilDone)
     } else if (input.temperature() > 20) {
         basic.setLedColors(0xff0000, 0xff0000, 0xff0000)
+        music.play(music.stringPlayable("B A G A G F A C5 ", 120), music.PlaybackMode.UntilDone)
     }
 })
